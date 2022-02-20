0679673fcd6e910cf7c37862e74c5e6aaf94f46b
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2022-02-20 16:15:39 -0300

    Baseline

---
+++ WaitDatabaseConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using MySql.Data.MySqlClient;
+
+var connection = new MySqlConnection("server=db;database=dbexemplo;uid=usuario;pwd=senha");
+connection.Open();
+
+Console.WriteLine($"Conectado com sucesso | MySQL version : {connection.ServerVersion}");
+
+connection.Close();
